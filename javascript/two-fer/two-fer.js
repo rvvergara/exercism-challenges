d79d64f14100cbc47341bbb2fb0f74d6add0239a
DESKTOP-7RP6RR3\dipto <diptokmk47@gmail.com>
2018-09-12 07:58:17 +0600

    Add two-fer
    Add a new solution of twoFer problem.

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,7 +1,8 @@
-let twoFer = name => {
-    let other;
-    name === undefined || name.length === 0 ? other = "you" : other = name;
-    return `One for ${other}, one for me.`;
+export let twoFer = name => {
+  let other;
+  name === undefined || name.length === 0 ? (other = "you") : (other = name);
+  return `One for ${other}, one for me.`;
+};
+export function twoFer2(name) {
+  return name ? `One for ${name}, one for me.` : "One for you, one for me.";
 }
-
-export default twoFer;
