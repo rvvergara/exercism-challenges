4b3cfb940346028333dbac85153e56f199f107ce
Ryan <6015315+rvvergara@users.noreply.github.com>
2018-09-15 12:07:54 +0800

    Redo commit to just add recursive versions
    - Initially just replaced all code with the recursive versions but here
    pasted them below the non-recursive ones

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,3 +1,41 @@
+export function encode(str) {
+  let encodeStr = "";
+  let key = str[0];
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (key === str[i]) {
+      counter++;
+      if (i + 1 === str.length) {
+        counter === 1 ? (encodeStr += key) : (encodeStr += counter + key);
+      }
+    } else {
+      counter === 1 ? (encodeStr += key) : (encodeStr += counter + key);
+      counter = 0;
+      key = str[i];
+      i--;
+    }
+  }
+  return encodeStr;
+}
+export function decode(str) {
+  let numbers = "";
+  let decodeSTR = "";
+  for (let i = 0; i < str.length; i++) {
+    if (/^\d+$/.test(str[i])) {
+      numbers += str[i];
+    } else {
+      let num = numbers === "" ? 1 : parseInt(numbers);
+      for (let j = 0; j < num; j++) {
+        decodeSTR += str[i];
+      }
+      numbers = "";
+    }
+  }
+  return decodeSTR;
+}
+
+// Recursive versions
+
 export function encode_recursive(str) {
   let key = str[0],
     count = 0,
