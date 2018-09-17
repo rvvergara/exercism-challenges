5277faf105be53f39e0d06c75490b6c97d2cbb8e
Ryan <6015315+rvvergara@users.noreply.github.com>
2018-09-17 14:15:46 +0800

    Add alternative solution
    -alternative solution uses for loop instead of recursion

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,3 +1,4 @@
+// Recursive solution
 export function toRna(DNA) {
     let complement = {
         G: "C",
@@ -8,4 +9,22 @@ export function toRna(DNA) {
     if (DNA === "") return "";
     if (complement[DNA[0]] === undefined) throw new Error('Invalid input DNA.');
     return complement[DNA[0]] + toRna(DNA.substr(1));
+}
+
+// For loop solution
+
+export function toRna(DNA) {
+  let complement = {
+      G: "C",
+      C: "G",
+      T: "A",
+      A: "U"
+    },
+    RNA = "";
+  if (DNA === "") return "";
+  for (let i = 0; i < DNA.length; i++) {
+    if (complement[DNA[i]] === undefined) throw new Error("Invalid input DNA.");
+    RNA += complement[DNA[i]];
+  }
+  return RNA;
 }
