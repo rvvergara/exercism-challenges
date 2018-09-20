c3c0c4e35e7036dd0982ced8d6fc5bab538fc27e
Ryan <6015315+rvvergara@users.noreply.github.com>
2018-09-20 08:30:21 +0800

    Redo Armstrong Numbers with two solutions
    1. Purely for-loop solution
    2. Purely recursive solution

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,3 +1,4 @@
+// For Loop Solution
 export function validate(num) {
     let n = Math.log(num) * Math.LOG10E + 1 | 0,
         beg = num,
@@ -6,9 +7,26 @@ export function validate(num) {
         remainder = num % divisor;
     for (let i = n; i > 0; i--) {
         let divisor = Math.pow(10, i - 1),
-            remainder = beg % divisor;
-        output += Math.pow((beg - remainder) / Math.pow(10, i - 1), n);
-        beg = num % Math.pow(10, i - 1);
+            remainder = beg % divisor,
+            digit = (beg - remainder) / divisor;
+        output += Math.pow(digit, n);
+        beg = remainder;
     }
     return output === num;
 }
+
+// Recursive solution
+export let validate2 = num => extract(num) === num;
+
+function extract(num, arg1) {
+    let n = Math.log(num) * Math.LOG10E + 1 | 0,
+        exp = arg1 === undefined ? n : arg1,
+        divisor = Math.pow(10, n - 1),
+        remainder = num % divisor,
+        digit = (num - remainder) / divisor;
+
+    return remainder === 0 ? Math.pow(digit, exp) : Math.pow(digit, exp) + extract(remainder, exp);
+}
+
+validate(153);
+validate2(153);
