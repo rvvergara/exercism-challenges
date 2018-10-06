6653e5546e25d0886a18fd63e4ae4aa9d0856d35
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-06 08:25:34 +0800

    Add etl exercise

---
+++ javascript/etl/etl.js
@@ -0,0 +1,14 @@
+let transform = (old) => {
+    let newWay = {};
+
+    for (let score in old) {
+        old[score].forEach(letter => {
+            newWay[letter.toLowerCase()] = Number(score);
+        });
+    }
+
+    return newWay;
+
+}
+
+export default transform;
