f72aa820ecb920c3ef1a628ccf3f3f4bdd3e1e32
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-08 09:21:49 +0800

    Refactor etl.js to shorten

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,14 +1,11 @@
-let transform = (old) => {
+let transform = old => {
     let newWay = {};
-
     for (let score in old) {
         old[score].forEach(letter => {
             newWay[letter.toLowerCase()] = Number(score);
         });
     }
-
     return newWay;
-
 }
 
 export default transform;
